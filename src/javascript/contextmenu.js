a8d52e1bcbeb86a81010334a11311d55ca9a21ab
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-17 21:38:44 +0900

    add : contextmenu 클릭 효과 추가, 색상 관련 미완성

--- src/javascript/contextmenu.js
+++ src/javascript/contextmenu.js
@@ -1,48 +1,75 @@
 export default class Contextmenu {
   constructor(target) {
     this.target = target;
+    this.selected = null;
     this.self = null;
     this.init();
   }
   init() {
     this.initMenu();
     this.render();
-    this.target.addEventListener('contextmenu', e => this.handleContextmenuEvent(e));
+    this.createEvent();
   }
   initMenu() {
     this.menu = [
       { name : '폴더 생성', fn : () => this.addFolder() },
       { name : '파일 생성', fn : () => this.addFile() },
-      { name : '이름 변경', fn : () => this.updateName() },
       { name : '글자 색상변경', fn : () => this.updateColor() },
       { name : '배경 색상변경', fn : () => this.updateBgColor() },
       { name : '테두리 색상변경', fn : () => this.updateBdColor() },
-      { name : '삭제', fn : () => this.delete() },
+      { name : '삭제', fn : () => this.deleteItem() },
     ];
   }
   render() {
     const ul = document.createElement('ul');
     ul.classList.add('contextmenu', 'popup', 'hidden');
-    ul.insertAdjacentHTML('beforeend', this.menu.reduce((acc, item) => acc += `<li>${item.name}</li>`, ''));
+    ul.insertAdjacentHTML('beforeend', this.menu.reduce((acc, item, index) => acc += `<li data-index="${index}">${item.name}</li>`, ''));
     this.target.append(ul);
     this.self = ul;
   }
-  handleContextmenuEvent(e) {
-    const item = e.target.closest('.item');
-    if(item) {
-      // 1. item의 dataset을 가져옴
-      // 2. 생성, 변경 등으로 정보 업데이트
-      // 3. 로컬 스토리지에 변경 값 저장
-      const obj = { ...item.dataset };
-      console.log(obj);
-      // console.log(localStorage.getItem('memotree'));
-      e.preventDefault();
-      e.stopPropagation();
-      this.showFileContext(e.pageX - 48, e.pageY - 120);
-    }
+  createEvent() {
+    this.self.addEventListener('click', e => this.handleMenuEvent(e.target.dataset.index));
+  }
+  handleMenuEvent(index) {
+    this.menu[index].fn();
+    this.hideFileContext();
+  }
+  addFolder() {
+    this.selected.addChild('folder');
+  }
+  addFile() {
+    this.selected.addChild('file');
+  }
+  updateColor() {
+    alert('만들고 있어요...');
+  }
+  updateBgColor() {
+    alert('만들고 있어요...');
+  }
+  updateBdColor() {
+    alert('만들고 있어요...');
+  }
+  deleteItem() {
+    this.selected.delete();
+  }
+  openMenu(e, target) {
+    this.selected = target;
+    e.stopPropagation();
+    e.preventDefault();
+    this.showFileContext(e.pageX - 48, e.pageY - 120);
   }
   showFileContext(x, y) {
+    if(this.selected.type === 'file') {
+      this.self.childNodes[0].classList.add('hidden');
+      this.self.childNodes[1].classList.add('hidden');
+    } else {
+      this.self.childNodes[0].classList.remove('hidden');
+      this.self.childNodes[1].classList.remove('hidden');     
+    }
     this.self.style.cssText = `left: ${x}px; top: ${y}px`;
     this.self.classList.remove('hidden');
+  }
+  hideFileContext() {
+    this.self.classList.add('hidden');
   }
 };
