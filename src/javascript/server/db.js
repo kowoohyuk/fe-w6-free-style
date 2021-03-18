cc5bf5f5a2e292b4e93f87c0bd3f8d3e2a1c2d86
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-18 19:21:02 +0900

    fix : add useNewUrlParser

--- src/javascript/server/db.js
+++ src/javascript/server/db.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
 export default mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@wooluck.c3ppg.mongodb.net/memotree?retryWrites=true&w=majority`,
   function(err) {
     if (err) {
