9a86b60d77942f07dc746a5df904abb7f3d542e8
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-18 19:46:31 +0900

    test : issue, .env 수정 필요

--- src/javascript/server/db.js
+++ src/javascript/server/db.js
@@ -5,9 +5,9 @@ dotenv.config();
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 console.log('*****');
-console.log(`mongodb+srv://admin:${process.env.DB_PASSWORD}@wooluck.c3ppg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+console.log(`mongodb+srv://admin:OrZMVvj1iYTCyuTG@wooluck.c3ppg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
 console.log('*****');
-export default mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@wooluck.c3ppg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+export default mongoose.connect(`mongodb+srv://admin:OrZMVvj1iYTCyuTG@wooluck.c3ppg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   function(err) {
     if (err) {
       console.error('mongodb connection error', err);
