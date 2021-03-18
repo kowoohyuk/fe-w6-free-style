d4efed526d87086c2e09ad5bc595942d118f2d7a
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-18 19:08:00 +0900

    test : db password change

--- src/javascript/server/db.js
+++ src/javascript/server/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export default mongoose.connect(`mongodb+srv://admin:1201202@wooluck.c3ppg.mongodb.net/memotree?retryWrites=true&w=majority`, 
+export default mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@wooluck.c3ppg.mongodb.net/memotree?retryWrites=true&w=majority`, 
   function(err) {
     if (err) {
       console.error('mongodb connection error', err);
