ce13c06635f2f782da4d5f355fac1c4861554900
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-18 19:18:24 +0900

    add useUnifiedTopology

--- src/javascript/server/db.js
+++ src/javascript/server/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-export default mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@wooluck.c3ppg.mongodb.net/memotree?retryWrites=true&w=majority`, 
+mongoose.set('useUnifiedTopology', true);
+export default mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@wooluck.c3ppg.mongodb.net/memotree?retryWrites=true&w=majority`,
   function(err) {
     if (err) {
       console.error('mongodb connection error', err);
