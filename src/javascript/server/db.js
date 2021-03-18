0ce1ad053cd9d5eb566253fda6b8aa452c9afee5
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-18 19:23:55 +0900

    fix : db 경로 변경

--- src/javascript/server/db.js
+++ src/javascript/server/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
-export default mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@wooluck.c3ppg.mongodb.net/memotree?retryWrites=true&w=majority`,
+export default mongoose.connect(`mongodb+srv://admin:${process.env.DB_PASSWORD}@wooluck.c3ppg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   function(err) {
     if (err) {
       console.error('mongodb connection error', err);
